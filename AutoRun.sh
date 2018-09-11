7044de52e21af16e32853efcf08f8b77a0073b48
Nick Rogness <nick@rogness.net>
2018-09-11 18:12:40 +0000

    Updated AutoRun to check for network connectivity before conituing

--- AutoRun.sh
+++ AutoRun.sh
@@ -1,4 +1,26 @@
-#!/usr/bin/env sh
+#!/bin/sh -e
 
-cd /home/pi/FehPlexSlideShow
-python3 FehPlexSlideShow.py
+is_network_alive() {
+
+    /bin/ping -c 2 -w 3 8.8.8.8 2>&1 > /dev/null
+    return $?
+}
+
+echo "Verifying Network is alive before continuing"
+
+ISALIVE="1"
+
+while [ "$ISALIVE" -ne "0" ]; do
+
+    is_network_alive
+    ISALIVE="$?"
+
+    echo "Waiting for network..."
+    sleep 3
+
+done
+
+echo "Network is alive, continuing"
+
+cd /home/pi/FehPlexSlideShow 
+/usr/bin/python3 /home/pi/FehPlexSlideShow/FehPlexSlideShow.py
